fc485c9e811b628eaef049dc2f13f01280854b44
lcrzlle <162576218+lcrzlle@users.noreply.github.com>
2026-06-04 15:20:26 +0200

    Create sanity.cli.ts

---
+++ sanity/sanity.cli.ts
@@ -0,0 +1,9 @@
+import { defineCliConfig } from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '30ojlbz1',
+    dataset: 'production'
+  },
+  studioHost: 'portfolio-lcrzlle'
+})
